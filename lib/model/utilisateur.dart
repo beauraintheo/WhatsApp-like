00054f86de43872fa92b19ccf7a0511047e00aea
Théo Beaurain <theobeaurain@mbp-de-theo.home>
2023-03-29 15:49:13 +0200

    udpate user schema

--- lib/model/utilisateur.dart
+++ lib/model/utilisateur.dart
@@ -1,32 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Utilisateur {
-  late String uid;
-  late String email;
+    late String uid;
+    late String email;
 
-  DateTime? birthday;
-  
-  String? pseudo;
-  String? lastname;
-  String? firstname;
-  String? avatar;
+    String? lastname;
+    String? firstname;
+    String? avatar;
 
-  Utilisateur.empty(){
-    uid = "";
-    email = "";
-  }
+    Utilisateur.empty(){
+        uid = "";
+        email = "";
+    }
 
-  Utilisateur(DocumentSnapshot snapshot){
-    uid = snapshot.id;
-    Map<String, dynamic> map = snapshot.data() as Map<String, dynamic>;
+    Utilisateur(DocumentSnapshot snapshot){
+        uid = snapshot.id;
+        Map<String, dynamic> map = snapshot.data() as Map<String, dynamic>;
 
-    Timestamp? timestamp = map["birthday"];
-    birthday = timestamp?.toDate();
-
-    pseudo = map["pseudo"];
-    email = map["email"];
-    lastname = map["lastname"];
-    firstname = map["firstname"];
-    avatar = map["avatar"];
-  }
+        email = map["email"];
+        lastname = map["lastname"];
+        firstname = map["firstname"];
+        avatar = map["avatar"];
+    }
 }
